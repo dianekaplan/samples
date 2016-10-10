b4f05470bf3727b713fb1d3852e67ed37f96b6b8
Diane Kaplan <dianekaplan@gmail.com>
2016-10-10 08:27:45 -0400

    added java file for rosalind problem

--- README.txt
+++ README.txt
@@ -1,7 +1,9 @@
 Code samples: 
 
-1. orf_finder_program.py- this is the simplest and is independently runable.  
+1. lcsm.java- my approach to Rosalind.org problem to find the longest common substring in a collection of sequences:  http://rosalind.info/problems/lcsm/
+
+2. orf_finder_program.py- this is the simplest and is independently runable.  
 The program is adapted from one of my homework problems, to take a DNA sequence and determine the open reading frames (aka ORFs, the part of the sequence that can be transcribed into proteins). I find the ORFs for the three distinct reading frames on both stands of the DNA. 
 
-2. find_splice_functions.py- there are more steps involved here, and these were helper functions for my class project (and not independently runable)- to strip out introns from a DNA sequence and return the cleaned string. 
+3. find_splice_functions.py- there are more steps involved here, and these were helper functions for my class project (and not independently runable)- to strip out introns from a DNA sequence and return the cleaned string. 
 
---
+++ lcsm.java
@@ -0,0 +1,99 @@
+package rosalind;
+import java.util.List;
+
+/**
+ * Created by Diane Kaplan on 10/9/2016.
+
+ Rosalind problem lcsm: Find the longest common shared motif among a list of fasta sequences
+ http://rosalind.info/problems/lcsm/
+ */
+
+public class lcsm {
+
+    public static void main(String[] args) {
+
+        // Use fasta_utils to extract list of sequences from fasta file
+        String file_to_use = "C:\\Users\\Diane\\Documents\\GitHub\\playtime\\JavaPlaytime\\src\\src\\rosalind\\dna.txt";
+        List seq_list = rosalind.fasta_utils.get_seq_strings(file_to_use);
+
+        System.out.println("Longest common substring is: " + find_longest_substring(seq_list));
+    }
+
+    // Go through a list of sequences and return the longest common substring
+    private static String find_longest_substring(List<String> list) {
+
+        boolean DEBUG = false;
+
+        int sequence_count = list.size();
+        if (DEBUG) {System.out.println("Our list has: " + sequence_count + " strings:" + list);}
+
+        String winning_substring = "";
+        boolean found_the_answer = false;
+
+        // Use shortest sequence in the bunch to identify substrings
+        String shortest_seq = find_shortest_seq(list);
+        int size_of_shortest_seq = shortest_seq.length();
+
+        // We'll try one size at a time, from 2bp up to length of the shortest seq (best case repeat)
+        // When you find a common string of one size, save and go to the next size up
+        // Return the longest that we successfully found in all of the sequences
+
+        // Try one size at a time
+        for (int i = 2; (i < size_of_shortest_seq) && !found_the_answer; i++) {
+
+            // Go base by base to find a snippet of this size, until we find one contained in all sequences
+            for (int j = 0; j < (size_of_shortest_seq - i); j++) {
+                String temp_snippet = shortest_seq.substring(j, j + i);
+
+                // For this snippet, test whether it is found in all the other strings
+                // If so, save it as our winner, break, and go to the next size
+                if (check_all_strings (temp_snippet, list )) {
+                    winning_substring = temp_snippet;
+                    if (DEBUG) {System.out.println(temp_snippet + " is a winner");}
+                    i++;
+                }
+                // If we reach the end of the first string without a winner for that length,
+                // we won't improve and our saved choice is the best
+                found_the_answer = true;
+            }
+        }
+        return winning_substring;
+    }
+
+    // Check to see whether a snippet is contained in all the strings of a list
+    private static boolean check_all_strings(String snippet, List<String> list) {
+
+        int sequence_count = list.size();
+        boolean is_found_everywhere = true;
+
+        for (int i = 1; (i < sequence_count) && is_found_everywhere; i++) {
+
+            String this_seq = list.get(i);
+            int found_at = this_seq.lastIndexOf(snippet);
+
+            if (found_at >= 0) {
+            } else {
+                is_found_everywhere = false;
+                break; // if it's missing from any, stop the search
+            }
+        }
+        return is_found_everywhere;
+    }
+
+
+    // In a list of sequences, return the shortest one
+    private static String find_shortest_seq(List<String> list) {
+
+        // initialize to the first string and compare/replace from there
+        String shortest_seq = list.get(0);
+        int shortest_seq_len = shortest_seq.length();
+
+        for (String str : list) {
+            if (str.length() < shortest_seq_len) {
+                shortest_seq_len = str.length();
+            }
+        }
+        return shortest_seq;
+    }
+
+}
